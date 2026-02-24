8395baa05bb89bdf932cef6e9a45b0b24e301ac1
vashonai <vashonai21@gmail.com>
2026-02-24 16:51:53 -0500

    feat: Add Providers component to wrap application with PlatformProvider.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import React from "react";
-import { PlatformProvider } from "./contexts/PlatformContext";
+import React from 'react';
+import { PlatformProvider } from './contexts/PlatformContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <PlatformProvider>{children}</PlatformProvider>;
